68f4294160f33adad36721856b3b03c4acb5540a
Aario <AarioAi@gmail.com>
2026-04-17 22:26:45 +0800

    update aa-posix-lib.sh md5 value

---
+++ gitpush.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+set -euo pipefail
+
+# https://github.com/aarioai/opt
+. /opt/aa/lib/aa-posix-lib.sh
+
+HERE="$(AbsDir "${BASH_SOURCE[0]}")"
+readonly HERE
+
+
+ # 生成 aa-posix-lib.sh hash 值
+generateAaPosixLibHash(){
+    local dir="${HERE}/aa/lib"
+    local old
+    old="$(find "$dir" -maxdepth 1 -name "md5-*" -type f -printf "%f\n" | head -1)"
+    # 删除之前的md5文件
+    find "$dir" -maxdepth 1 -name "md5-*" -type f -delete
+    local hash
+    hash=$(md5sum "${dir}/aa-posix-lib.sh" | awk '{print $1}')
+    local new="md5-${hash}"
+    touch "${dir}/$new"
+    if [ "$old" != "$new" ]; then
+        Info "aa-posix-lib.sh changed from ${old} => ${hash}"
+    fi
+}
+
+main(){
+  Usage $# -le 1 './gitpush.sh [comment]'
+  local comment="${1:-"no comment"}"
+
+  generateAaPosixLibHash
+
+  # 将 CRLF 转为 LF
+  git config core.autocrlf false
+  git add -A .
+  git commit -m "$comment"
+  git push origin main
+}
+
+main "$@"
